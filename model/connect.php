f7fbc505a1006da69a42a1251570071935415373
judoittt <your_email@example.com>
2025-03-30 16:03:43 +0700

    change1

--- model/connect.php
+++ model/connect.php
@@ -3,16 +3,16 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 class Connect {
     public function openConnect() {
-        $host = "localhost"; // Hoặc đúng MySQL Host bạn tìm thấy trong cPanel
-        $user = "admin2"; 
-        $pass = "admin2"; 
+        $host = "localhost"; 
+        $user = "admin1"; 
+        $pass = "admin1"; 
         $db   = "quanlybanhang"; 
         $port = 3307;
        
 
         $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
-        // Kiểm tra kết nối có lỗi hay không
+     
         if (!$conn) {
             die("Kết nối thất bại: " . mysqli_connect_error());
         } 
--- model/typeOfProduct.php
+++ model/typeOfProduct.php
@@ -5,7 +5,7 @@
         public function selectAllTOP(){
             $p = new Connect();
             $conn = $p->openConnect();
-            $sql = "SELECT * FROM danhmucsanpham";
+            $sql = "SELECT * FROM danhmuc";
             $result = $conn->query($sql);
             return $result;
         }
